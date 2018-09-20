6b067fc7bef1084151fa49208abfe8d0a8d7066c
jsantos93 <jcas@cesar.school>
2018-09-20 15:10:08 -0300

    Create struct

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+typedef struct Node
+{
+    int value;
+    struct Node_ *left;
+    struct Node_ *right;
+    struct Node_ *father;
+} node;
+
 int main(void)
 {
     return 0;
